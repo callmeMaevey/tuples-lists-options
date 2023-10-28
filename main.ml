9dc8469f92f8b8f009eaa2a4464847b15ff32d95
Maevey <70716966+callmeMaevey@users.noreply.github.com>
2023-10-27 22:40:28 -0400

    added comments for future me

--- main.ml
+++ main.ml
@@ -6,4 +6,18 @@ let (x,y) = tup
 
 let (a,b,c) = another_tup
 
+(* error explination
+
+	has type int * string * float 
+	but expected 'a * 'b 
+
+	I think those are read alpha beta
+	but not sure
+
 let (i,j) = another_tup
+
+	this is expected I just wanted to see what it would do
+*)
+
+
+
